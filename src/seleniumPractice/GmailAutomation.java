ecdc80da363e4768d61721e1c1fc39860593a284
NagKothapalli <nag022@gmail.com>
2022-07-08 10:33:54 +0530

    July8th code pushed

--- src/seleniumPractice/GmailAutomation.java
+++ src/seleniumPractice/GmailAutomation.java
@@ -13,17 +13,29 @@ import junit.framework.Assert;
 public class GmailAutomation
 {
 	String expectedTitle = "Gmail";
+	WebDriver driver; //null
+	public GmailAutomation()
+	{
+		System.out.println("GmailAutommation Default Constructor");
+		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\JarFiles\\chromedriver-win32-90\\chromedriver.exe");
+		driver = new ChromeDriver(); //1234
+	}
+	//java.lang.NullPointerException
+
 	//java.lang.IllegalStateException: The path to the driver executable must be set by the webdriver.chrome.driver system property
 	
 	//org.openqa.selenium.SessionNotCreatedException: session not created: This version of ChromeDriver only supports Chrome version 103
 	    //Current browser version is 90.0.4430.212 with binary path C:\Program Files 
+	
+	//org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {"method":"css selector","selector":"#identifierId"}
+	
+	//org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {"method":"css selector","selector":"#abcdefgh"}
+	
 	@Test
-	public void launchApplication()
+	public void launchApplication() //Test1
 	{
-		System.out.println("Launch Application");
-		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\JarFiles\\chromedriver-win32-90\\chromedriver.exe");
-		//ChromeDriver driver = new ChromeDriver();//opens an empty chrome browser window //Session ID
-		WebDriver driver = new ChromeDriver();		
+		System.out.println("Launch Application");		
+		//ChromeDriver driver = new ChromeDriver();//opens an empty chrome browser window //Session ID				
 		driver.get("https://gmail.com");
 		String session = driver.getWindowHandle();
 		System.out.println("window session ID:"+session);
@@ -32,15 +44,144 @@ public class GmailAutomation
 		String actualTitle = driver.getTitle();
 		System.out.println("Actual Title :" + actualTitle);
 		Assert.assertEquals(expectedTitle, actualTitle); //Validation or Assertion
-		//WebElement emailObj = driver.findElement(By.name("identifier")); //identifierId
+		//WebElement emailObj = driver.findElement(By.name("identifier")); //identifierId		
+	}
+	@Test
+	public void loginToApplication() //Test2
+	{
+		launchApplication();
 		WebElement emailObj = driver.findElement(By.id("identifierId"));
 		emailObj.click();
 		emailObj.sendKeys("dsfdsgfgfghgfhgg");
 		emailObj.clear();
 		emailObj.sendKeys("nag022");
 		List<WebElement> elements = driver.findElements(By.className("VfPpkd-vQzf8d"));
-		WebElement element = elements.get(1);
-		element.click();
+		//WebElement element = elements.get(1);		
+		//element.click();
+		for(int i=0;i<elements.size();i++)
+		{
+			String txt = elements.get(i).getText();
+			System.out.println("Objects searched :" + txt);
+			if(txt.equals("Next"))
+			{
+				elements.get(i).click();
+				break;
+			}
+		}
 	}
+	//Execution Sequence : Constructor  -> Launch -> ForGotEmail
+	@Test
+	public void forgotMyEmail()
+	{
+		launchApplication();
+		System.out.println("User ForGot his Email");
+		//driver.findElement(By.tagName("button")).click(); //Forgot email? Create account
+		List<WebElement> elements = driver.findElements(By.tagName("button"));
+		for(int i=0;i<elements.size();i++)
+		{
+			String txt = elements.get(i).getText();
+			if(txt.equals("Forgot email?"))
+			{
+				elements.get(i).click();
+				break;
+			}
+		}
+	}
+	
+	@Test
+	public void createMyAccount()
+	{
+		launchApplication();
+		System.out.println("User ForGot his Email");
+		//driver.findElement(By.tagName("button")).click(); //Forgot email? Create account
+		List<WebElement> elements = driver.findElements(By.tagName("button"));
+		for(int i=0;i<elements.size();i++)
+		{
+			String txt = elements.get(i).getText();
+			System.out.println("Objects searched :" + txt);
+			if(txt.equals("Create account"))
+			{
+				elements.get(i).click();
+				break;
+			}
+		}
+	}
+	
+	@Test
+	public void userLearnMore()
+	{
+		launchApplication();
+		System.out.println("User ForGot his Email");
+		//driver.findElement(By.tagName("button")).click(); //Forgot email? Create account
+		List<WebElement> elements = driver.findElements(By.tagName("a"));
+		for(int i=0;i<elements.size();i++)
+		{
+			String txt = elements.get(i).getText();
+			System.out.println("Objects searched :" + txt);
+			if(txt.equals("Learn more"))
+			{
+				elements.get(i).click();
+				break;
+			}
+		}
+	}
+	
+	@Test
+	public void help()
+	{
+		launchApplication();
+		System.out.println("User ForGot his Email");
+		//driver.findElement(By.tagName("button")).click(); //Forgot email? Create account
+		List<WebElement> elements = driver.findElements(By.tagName("a"));
+		for(int i=0;i<elements.size();i++)
+		{
+			String txt = elements.get(i).getText();
+			System.out.println("Objects searched :" + txt);
+			if(txt.equals("Help"))
+			{
+				elements.get(i).click();
+				break;
+			}
+		}
+	}
+	@Test
+	public void privacy()
+	{
+		launchApplication();
+		System.out.println("User ForGot his Email");
+		//driver.findElement(By.tagName("button")).click(); //Forgot email? Create account
+		List<WebElement> elements = driver.findElements(By.tagName("a"));
+		for(int i=0;i<elements.size();i++)
+		{
+			String txt = elements.get(i).getText();
+			System.out.println("Objects searched :" + txt);
+			if(txt.equals("Privacy"))
+			{
+				elements.get(i).click();
+				break;
+			}
+		}
+	}
+	@Test
+	public void terms()
+	{
+		launchApplication();
+		System.out.println("User ForGot his Email");
+		//driver.findElement(By.tagName("button")).click(); //Forgot email? Create account
+		List<WebElement> elements = driver.findElements(By.tagName("a"));
+		for(int i=0;i<elements.size();i++)
+		{
+			String txt = elements.get(i).getText();
+			System.out.println("Objects searched :" + txt);
+			if(txt.equals("Terms"))
+			{
+				elements.get(i).click();
+				break;
+			}
+		}
+	}
+	
+	
+	
 
 }
